60667ee8e9cf446024e734221d0ee5d7aa9579f1
VinayMoolya <72350404+VinayMoolya@users.noreply.github.com>
2023-11-23 16:30:17 +0530

    Changes

--- aicounsellor/public/dataaugment.py
+++ aicounsellor/public/dataaugment.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
 
-# df = pd.read_json(
-#     '/Users/vinaymoolya/Desktop/projects/AI_PROJ/mtech_final.json')
+df = pd.read_json(
+    "/Users/vinaymoolya/Desktop/projects/AI_PROJ/aicounsellor/public/data/btech.json")
 
 # df.loc[df['Package'] > 5000000, 'Package'] = df['Package']/2
 
@@ -17,8 +17,22 @@ import pandas as pd
 # NIT - 0.75
 # Top Private COlleges - 0.5
 # Rest - 0.25
+# for row in df.rows():
+#     if row['IT'] == -1:
+#         row['IT'] = 250
+#     else:
+#         row['IT'] = row['IT']
+#     if row['NIRF'] == -1:
+#         row['NIRF'] = 250
+#     else:
+#         row['NIRF'] = row['NIRF']
 
-# df['Score'] = df['Score']-10
+df['IT'] = 250 if df['IT'] == -1 else df['IT']
+df['NIRF'] = 250 if df['NIRF'] == -1 else df['NIRF']
+
+print(df)
+# df['Score'] = 0.3*(20/df['Id']) + 0.2*df['ROI'] + \
+# 0.5((50/df['IT']) + (50/df['NIRF']))
 
 # now we also need to add what exams the students are supposed to write to join that college
 
@@ -41,4 +55,4 @@ import pandas as pd
 # print(df['Score'].max())
 # print(df)
 
-# df.to_json('mtech_final1.json', orient='records')
+# df.to_json('mtech_final.json', orient='records')
